05ea40f09e976181f27c06efe72c7ea4af062495
Sébastien BEAU <sebastien.beau@akretion.com>
2019-09-13 19:02:01 +0200

    [IMP] migrate paypal backport

--- invader_payment_paypal/__manifest__.py
+++ invader_payment_paypal/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Paypal Payment Acquirer (REST, Base)",
     "summary": "REST Services for Stripe Payments (base module)",
-    "version": "12.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
     "author": "Akretion",
